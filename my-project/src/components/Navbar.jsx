60ed603e1fcec860f95365e724697143bc9b6c89
Naveenraj20 <naveenrajkingraj@gmail.com>
2024-08-12 13:56:49 +0530

    app

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -5,28 +5,13 @@ import Login from "./Login";
 import { Link } from "react-router-dom";
 
 export default function Navebar() {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-  );
+  
   const [searchEle, setSearchEle] = useState("search");
   const [value, setValue] = useState(
     localStorage.getItem("serValue")
       ? localStorage.getItem("serValue")
       : searchEle
   );
-  const element = document.documentElement;
-
-  useEffect(() => {
-    if (theme === "dark") {
-      element.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      document.body.classList.add("dark");
-    } else {
-      element.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      document.body.classList.remove("dark");
-    }
-  }, [theme]);
 
   useEffect(() => {
     localStorage.setItem("serValue", searchEle);
@@ -51,7 +36,7 @@ export default function Navebar() {
     <>
       <li>
         <a
-          className="dark:hover:bg-slate-100 dark:text-black md:dark:text-white dark:hover:text-black"
+          className="  dark:text-black md:dark:text-white dark:hover:text-black"
           href="/"
         >
           Home
@@ -75,7 +60,7 @@ export default function Navebar() {
       </li>
       <li>
         <a
-          className="dark:hover:bg-slate-100 dark:text-black md:dark:text-white dark:hover:text-black"
+          className=" dark:hover:bg-slate-100 dark:text-black md:dark:text-white dark:hover:text-black"
           href=""
         >
           About
@@ -83,34 +68,7 @@ export default function Navebar() {
       </li>
     </>
   );
-  const themeBut = (
-    <>
-      <label className="swap swap-rotate ">
-        {/* this hidden checkbox controls the state */}
-        <input type="checkbox" className="theme-controller" value="synthwave" />
-
-        {/* sun icon */}
-        <svg
-          className="swap-off fill-current w-7 h-7"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        >
-          <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-        </svg>
-
-        {/* moon icon */}
-        <svg
-          className="swap-on fill-current w-7 h-7"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-        </svg>
-      </label>
-    </>
-  );
+  
 
   function handleClick(e) {
     setSearchEle(e);
@@ -119,9 +77,9 @@ export default function Navebar() {
   return (
     <>
       <div
-        className={`max-w-screen-2xl z-40 dark:bg-slate-900 container mx-auto md:px-20 px-4 top-0 sticky shadow-sm ${
+        className={`max-w-screen-2xl z-40 bg-slate-900 container mx-auto md:px-20 px-4 top-0 sticky shadow-sm ${
           sticky
-            ? "sticky-navbar shadow-md bg-slate-100 dark:bg-slate-800 transition-all duration-200 ease-in-out"
+            ? "sticky-navbar shadow-md bg-slate-700  transition-all duration-200 ease-in-out"
             : ""
         }`}
       >
@@ -153,7 +111,7 @@ export default function Navebar() {
                 className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
                 {navItems}
-                <a className="p-2 dark:text-black">{themeBut}</a>
+               
               </ul>
             </div>
             <a className="text-2xl">
@@ -167,8 +125,8 @@ export default function Navebar() {
               <ul className="menu menu-horizontal px-1">{navItems}</ul>
             </div>
 
-            <div className="mx-1 hidden md:block">
-              <label className="order rounded-md input flex items-center gap-2">
+            <div className="mx-1 hidden md:block ">
+              <label className="order rounded-md input flex items-center gap-2 dark:bg-white">
                 <form
                   onSubmit={(e) => {
                     e.preventDefault();
@@ -188,12 +146,12 @@ export default function Navebar() {
                   />
                 </form>
 
-                <Link to="/SearchComponent" className="">
+                <Link to="/SearchComponent" className="dark:bg-white">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 16 16"
                     fill="currentColor"
-                    className="w-4 h-4 opacity-70 dark:text-black"
+                    className="w-4 h-4 opacity-70 dark:text-black "
                   >
                     <path
                       fillRule="evenodd"
@@ -205,7 +163,6 @@ export default function Navebar() {
               </label>
             </div>
 
-            <div className="mx-2 w-fit hidden md:block">{themeBut}</div>
 
             <a
               className="btn bg-blue-400 text-white hover:bg-blue-600 duration-300"
